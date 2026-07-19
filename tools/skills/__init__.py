1866533a20f1f5f252afa785c6234671d1251920
jkwke <3045749287@qq.com>
2026-07-19 18:28:48 +0800

    Initialize skills module with imports and exports

---
+++ tools/skills/__init__.py
@@ -0,0 +1,11 @@
+from .get_time import get_time
+from .weather import get_weather
+from .launch_application import open_url
+from .web_search import web_search
+
+__all__ = [
+    'get_time',
+    'get_weather',
+    'open_url',
+    'web_search',
+]
